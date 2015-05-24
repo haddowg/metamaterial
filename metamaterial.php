ed79f3c2e4bbfe4c8e934fe364de384a9f21ffe2
haddowg <greg@greghaddow.co.uk>
2015-05-24 08:58:02 +0100

    remove debugging

--- metamaterial.php
+++ metamaterial.php
@@ -3,14 +3,10 @@
 if(!class_exists( 'HaddowG\MetaMaterial\Metamaterial' )) {
 	spl_autoload_register(
 		function ( $pClassName ) {
-            		print('src/' . strtolower( str_replace( "\\", "/", $pClassName ) ));
 			spl_autoload( 'src/' . strtolower( str_replace( "\\", "/", $pClassName ) ));
 		}
 	);
 
 	\HaddowG\MetaMaterial\Metamaterial::$default_assets_url = plugin_dir_url(__FILE__).'src/haddowg/metamaterial/';
 	\HaddowG\MetaMaterial\Metamaterial::$default_assets_path = plugin_dir_path(__FILE__).'src/haddowg/metamaterial/';
-
-	echo "\r\n" . \HaddowG\MetaMaterial\Metamaterial::$default_assets_url .  "\r\n";
-	echo "\r\n" . \HaddowG\MetaMaterial\Metamaterial::$default_assets_path .  "\r\n";
 }
