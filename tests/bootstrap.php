1b909c552c5d00c93307bdceca64648e5e360a92
haddowg <greg@greghaddow.co.uk>
2015-05-26 19:08:07 +0100

    adjust boostrap to boot patchwork early

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
     spl_autoload_extensions('.php');
     require dirname(dirname(__FILE__)).'/vendor/autoload.php';
-
+    WP_Mock::setUp();
     spl_autoload_register(
         function ( $pClassName ) {
             spl_autoload( dirname(dirname(__FILE__)).'/src/' . strtolower( str_replace( "\\", "/", $pClassName ) ));
